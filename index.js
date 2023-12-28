0d3a9c4babdbb6756fc64783834a646d3900fff4
Benny Wick <bwick@Bennys-MBP.home>
2023-12-27 21:48:30 -0800

    add more functionality

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {
 	ref,
 	push,
 	onValue,
-	remove,
+	update,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 // Google Real-time Database initialization
@@ -26,33 +26,65 @@ publishBtn.addEventListener("click", () => {
 		text: endorsementText.value,
 		from: fromInput.value,
 		to: toInput.value,
+		likes: 0,
 	};
 	push(endorsementListInDB, endorsement);
 });
 
 onValue(endorsementListInDB, (snapshot) => {
+	const likeBtn = document.getElementById("like-btn");
+
 	if (snapshot.exists()) {
-		let endorsmentHtml = "";
 		let itemArray = Object.entries(snapshot.val());
 
 		// Clear the list
 		endorsementList.innerHTML = "";
 
-		for (let i = 0; i < itemArray.length; i++) {
-			let currentItem = itemArray[i];
+		for (let i = itemArray.length - 1; i >= 0; i--) {
+			const currentItem = itemArray[i];
+			const endorsementId = currentItem[0];
 			const text = currentItem[1].text;
 			const from = currentItem[1].from;
 			const to = currentItem[1].to;
+			let numberOfLikes = currentItem[1].likes;
+			let hasLiked = localStorage.getItem(`${endorsementId}`);
 
-			endorsmentHtml += `
-            <div>
-                <p id="to" >To ${to}</p>
-                <p id="endorsement">${text}</p>
-                <p id="from">From ${from}</p>
-            </div>
-            `;
+			const endorsementItemDiv = document.createElement("div");
+			const fromAndLikeContainer = document.createElement("div");
+			const toText = document.createElement("p");
+			const textBody = document.createElement("p");
+			const fromText = document.createElement("p");
+			const likeBtn = document.createElement("button");
+
+			toText.textContent = `To ${to}`
+			toText.id = "to";
+			textBody.textContent = text;
+			textBody.id = "endorsement";
+			fromText.textContent = `From ${from}`;
+			fromText.id = "from";
+			likeBtn.textContent = `♥ ${numberOfLikes}`;
+
+			endorsementList.append(endorsementItemDiv);
+			endorsementItemDiv.appendChild(toText);
+			endorsementItemDiv.appendChild(textBody);
+			endorsementItemDiv.appendChild(fromAndLikeContainer);
+			fromAndLikeContainer.appendChild(fromText);
+			fromAndLikeContainer.appendChild(likeBtn);
+
+			if (hasLiked === 'true') {
+                likeBtn.className = 'liked-style'
+			} else {
+				likeBtn.addEventListener("click", () => {
+					let exactLocationOfItemInDB = ref(
+						database,
+						`endorsementList/${endorsementId}`
+					);
+					localStorage.setItem(`${endorsementId}`, JSON.stringify(true));
+					update(exactLocationOfItemInDB, { likes: numberOfLikes++ });
+				});
+			}
 		}
-
-		endorsementList.innerHTML = endorsmentHtml;
-	}
+	} else {
+        console.log('nothing yet')
+    }
 });
